6fea24d4ed9b2f6851d0447074f44cdbc40dbb2a
RJ MacReady <82rjmacready@gmail.com>
2014-07-06 07:36:06 -0700

    removed bin from git

--- dbmain.c
+++ dbmain.c
@@ -3,7 +3,21 @@
 
 #include "db.h"
 
+void help(int argc, char** argv) {
+	fprintf(stderr, "Usage: \n");
+	fprintf(stderr, "  %s -c <dbname>            creates db\n", argv[0]);
+	fprintf(stderr, "  %s -o <dbname>            opens-closes db\n", argv[0]);
+	fprintf(stderr, "  %s -p <dbname> <string>   opens-puts-closes db\n", argv[0]);
+	fprintf(stderr, "  %s -g <dbname> <id>       opens-gets-closes db\n", argv[0]);
+
+	exit(0);
+}
+
 int main(int argc, char** argv) {
+	if(argc == 1) {
+		help(argc, argv);
+	}
+	
 	char action = argv[1][0];
 	char* name = argv[2];
 	struct db db;
@@ -18,7 +32,11 @@ int main(int argc, char** argv) {
 		}
 	}
 	
-	if(action == 'o' || action == 'p' || action == 'g') {		
+	if(action == 'o' || action == 'p' || action == 'g') {
+		if(argc < 3) {
+			help(argc, argv);
+		}
+		
 		if(0 == load_db(name, &db)) {
 			fprintf(stderr, "Loaded db\n");
 		} else {
@@ -28,6 +46,10 @@ int main(int argc, char** argv) {
 	}
 	
 	if(action == 'p') {
+		if(argc < 4) {
+			help(argc, argv);
+		}
+		
 		char *data = argv[3];
 		size_t len = strlen(data);
 		
@@ -40,6 +62,10 @@ int main(int argc, char** argv) {
 	}
 	
 	if(action == 'g') {
+		if(argc < 4) {
+			help(argc, argv);
+		}
+		
 		char *data;
 		size_t len;
 		id = strtoll(argv[3], NULL, 10);
